0ba1f6fc27e6d33ae2ead6675c7ce614ad18d959
raxgomez <rafa_gomez24@hotmail.com>
2021-11-11 10:16:24 -0600

    Nuevo Script
    Se agregó un nuevo script para prueba que no hace nada. Saludos

---
+++ Practica/Assets/Rafael/Scriptparaborrar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptparaborrar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*Este es un script de prueba no hace nada es únicamente de prueba.
+        No le hagan caso
+        Saludos
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
